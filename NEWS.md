b49fca2ad5852c00ec7fff8094a3a7cd47325e57
Julian Straus <julian.straus@sintef.no>
2025-03-25 14:59:22 +0100

    Initialize the repository with a basic Julia pkg

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# Release notes
+
+## Version 0.1.0 (2025-03-xx)
+
+* Initial version of the package
---
+++ src/EnergyModelsCompliance.jl
@@ -0,0 +1,5 @@
+module EnergyModelsCompliance
+
+greet() = print("Hello World!")
+
+end # module EnergyModelsCompliance
